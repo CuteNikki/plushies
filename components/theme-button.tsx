64924d2d419acf672e1b2ad245c2d7942b24e396
Nikki <bravonikki13@gmail.com>
2026-09-23 21:55:12 +0200

    feat: add icons to theme dropdown

--- components/theme-button.tsx
+++ components/theme-button.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
+
+import { MonitorIcon, MoonIcon, SunIcon } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -18,19 +19,22 @@ export function ThemeButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon'>
-          <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+          <SunIcon className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
         <DropdownMenuItem onClick={() => setTheme('light')}>
+          <SunIcon className='size-4 shrink-0' />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>
+          <MoonIcon className='size-4 shrink-0' />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
+          <MonitorIcon className='size-4 shrink-0' />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
